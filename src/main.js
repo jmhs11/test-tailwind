dcb21b40750ce70d18672a60ff942c149434ca64
jmhs11 <josemariahs@gmail.com>
2022-06-08 10:02:29 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "/dist/output.css";
+
+document.querySelector("#app").innerHTML = `
+<h1 class="text-3xl font-bold underline text-red-500">
+Hello world!
+</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
